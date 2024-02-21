211cd5368087db3c95c0ed2d78100e80ae8309af
AA <Student@CAB4-2>
2024-02-21 14:52:27 +1000

    vers 2/ cut image/

--- PeopleLocation/MainWindow.xaml.cs
+++ PeopleLocation/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Drawing;
+using System.Linq;
 using System.Net.Http.Json;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -23,11 +24,16 @@ namespace PeopleLocation
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        List<Canvas> canvasList = new List<Canvas>();
 
-        public List<Person> person { get; set; }
+        public List<Person> person { get; set; } = new();
         public MainWindow()
         {
             InitializeComponent();
+
+            canvasList.Add(grid.FindName("canvas00") as Canvas);
+            for (int i = 0; i < 23; i++)
+                canvasList.Add(grid.FindName("canvas" + i) as Canvas);
             DataContext = this;
             DrawLocation();
         }
@@ -51,54 +57,102 @@ namespace PeopleLocation
             {
                 Thread.Sleep(stopTime);
 
-                Dispatcher.Invoke(() =>
+                try
                 {
-                    isFirst = false;
-                    DrawSetFrontView();
-                    stopTime = 3000;
-                });
+                    Dispatcher.Invoke(() =>
+                    {
+                        //isFirst = false;
+                        DrawSetFrontView();
+                        stopTime = 3000;
+                    });
+                }
+                catch (Exception ex) { }
             }
         }
 
         private void DrawSetFrontView()
         {
             GetData();
-            var EllipsDoctor = new Ellipse()
+            Canvas c = null;
+            for (int i = 0; i < canvasList.Count; i++)
             {
-                Width = 5,
-                Height = 5,
-                StrokeThickness = 2,
-                Fill = Brushes.Blue
-            }; 
-            var EllipsClient = new Ellipse()
+                canvasList[i].Children.Clear();
+            }
+
+            foreach (var p in person)
             {
-                Width = 5,
-                Height = 5,
-                StrokeThickness = 2,
-                Fill = Brushes.Green
-            };
-           foreach(var p in person)
-            {
-                 var c = grid.FindResource("canvas" + p.LastSecurityPointNumber) as Canvas;
-                 if (p.personRole == "сотрудник")
-                 {
-                     c.Children.Add(EllipsDoctor);
-                     SetPosition(c);
-                 }
-                 else
-                 {
-                     c.Children.Add(EllipsClient);
-                     SetPosition(c);
-                 }
+                bool find = false;
+                for (int i = 0; i < canvasList.Count; i++)
+                {
+                    find = false;
+                   
+                    c = canvasList[i];
+
+                    if (c != null && c.Name == "canvas" + p.LastSecurityPointNumber && p.LastSecurityPointDirection == "in")
+                    {
+                        find = true;
+                        break;
+                    }
+                }
+                if (!find)
+                {
+                    c = canvas00;
+                }
+                if (p.personRole == "сотрудник")
+                {
+                    var EllipsDoctor = new Ellipse()
+                    {
+                        Width = 20,
+                        Height = 20,
+                        StrokeThickness = 2,
+                        Fill = Brushes.Blue
+                    };
+                    c.Children.Add(EllipsDoctor);
+                    SetPosition(c, EllipsDoctor);
+                }
+                else if (p.personRole == "клиент")
+                {
+                    var EllipsClient = new Ellipse()
+                    {
+                        Width = 20,
+                        Height = 20,
+                        StrokeThickness = 2,
+                        Fill = Brushes.Green
+                    };
+                    c.Children.Add(EllipsClient);
+                    SetPosition(c, EllipsClient);
+                }
             }
         }
 
-        private static void SetPosition(Canvas? c)
+        static Random Random = new Random();
+        private static void SetPosition(Canvas? c, Ellipse ellipse)
         {
-            Canvas.SetTop(c, c.ActualHeight - 10);
-            Canvas.SetBottom(c, 0);
-            Canvas.SetLeft(c, c.ActualWidth - 10);
-            Canvas.SetRight(c, 0);
+            SetRndPosition(c, ellipse);
+            Ellipse e;
+            int x1 = (int)Canvas.GetLeft(ellipse) + 10;
+            int y1 = (int)Canvas.GetTop(ellipse) + 10;
+            for (int i = 0; i < c.Children.Count; i++)
+            {
+                e = c.Children[i] as Ellipse;
+                if (e != ellipse)
+                {
+                    int x2 = (int)Canvas.GetLeft(e) + 10;
+                    int y2 = (int)Canvas.GetTop(e) + 10;
+
+                    if (Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)) < 20)
+                    {
+                        SetPosition(c, ellipse);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void SetRndPosition(Canvas? c, Ellipse ellipse)
+        {
+            Canvas.SetTop(ellipse, Random.Next(0, (int)c.ActualHeight - 20));
+            Canvas.SetLeft(ellipse, Random.Next(0, (int)c.ActualWidth - 20));
         }
 
         private async void GetData()
@@ -106,7 +160,8 @@ namespace PeopleLocation
             try
             {
                 person = new List<Person>(await Conn.Inst().GetFromJsonAsync<List<Person>>(""));
-            } catch(Exception e)
+            }
+            catch (Exception e)
             {
                 Message(e);
             }
